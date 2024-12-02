78cd8c801a4e077aa2fdf318485aeb56ce968e54
Anderson Lima <anderson.gadelha@zup.com.br>
2024-12-02 16:13:24 -0300

    Adicionado codigo para declarar variaveis e imprimir no console

---
+++ declarando-variaveis/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String nome = "Anderson Bruno";
+        int idade = 32;
+        double altura = 1.76;
+        boolean isMatriculado = true;
+        System.out.printf("Nome: " + nome);
+        System.out.printf(" Altura: " + altura);
+        System.out.printf(" Idade: " + idade);
+        System.out.printf(" Matriculado: " + isMatriculado);
+
+    }
+}
