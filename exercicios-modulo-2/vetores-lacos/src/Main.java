9bc2dd0fbcbcae026c370c279ede265fd4266009
Anderson Lima <anderson.gadelha@zup.com.br>
2024-12-05 11:07:33 -0300

    feat: adiciona calculo de idade

--- exercicios-modulo-2/vetores-lacos/src/Main.java
+++ exercicios-modulo-2/vetores-lacos/src/Main.java
@@ -1,8 +1,11 @@
+import java.time.LocalDate;
+import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //LAÇOS
         System.out.println("Laço for:");
         int[] numeros = {10, 21, 32, 43, 54};
         for (int i = 0; i < 5; i++) {
@@ -16,14 +19,17 @@ public class Main {
             i++;
         } while (i < 5);
 
+        //CALCULADORA IDADE
         Scanner scanner = new Scanner(System.in);
         String nome;
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        String dataNascimento;
+        LocalDate dataNascimento;
         System.out.println("Digite o seu nome: ");
         nome = scanner.nextLine();
-        System.out.println("Digite sua data de nascimento no formato dd/MM/yyyy: ");
-        dataNascimento = scanner.nextLine();
+        System.out.println("Digite sua data de nascimento no formato dd/MM/aaaa: ");
+        dataNascimento = LocalDate.parse(scanner.nextLine(), formatter);
+        LocalDate dataAtual = LocalDate.now();
         System.out.println(nome);
+        System.out.println(Period.between(dataNascimento, dataAtual).getYears() + " anos");
     }
 }
