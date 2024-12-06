9da7b2bd0b0e4518707dea35c74026230bd83f79
Anderson Lima <anderson.gadelha@zup.com.br>
2024-12-06 16:42:00 -0300

    Adiciona exercicio fibonacci

--- exercicios-modulo-2/java-basico/src/Main.java
+++ exercicios-modulo-2/java-basico/src/Main.java
@@ -6,104 +6,135 @@ public class Main {
         // 1 - Soma de Números
         Scanner scanner = new Scanner(System.in);
         int numero;
-        int resultado = 0;
-        System.out.println("Digite um numero para somar de 1 até:");
-        numero = scanner.nextInt();
-        for (int i = 1; i <= numero; i++) {
-            resultado = resultado + i;
-        }
-        System.out.println(resultado);
+//        int resultado = 0;
+//        System.out.println("Digite um numero para somar de 1 até:");
+//        numero = scanner.nextInt();
+//        for (int i = 1; i <= numero; i++) {
+//            resultado = resultado + i;
+//        }
+//        System.out.println(resultado);
+//
+//        // 2 - Fatorial
+//        System.out.println("Digite um numero para calcular o fatorial:");
+//        numero = scanner.nextInt();
+//        resultado = 1;
+//        do {
+//            resultado = resultado * numero;
+//            numero--;
+//        } while (numero > 1);
+//        System.out.println(resultado);
+//
+//        // 3 - Contagem de Dígitos
+//        System.out.println("Digite um numero para receber a quantidade de dígitos:");
+//        numero = scanner.nextInt();
+//        String numeroString = String.valueOf(Math.abs(numero));
+//        System.out.println(numeroString.length());
+//
+//        // 4 - Números Primos
+//        boolean primo = true;
+//        System.out.println("Digite um numero para saber se ele é primo:");
+//        numero = scanner.nextInt();
+//        if (numero < 2) {
+//            primo = false;
+//        } else {
+//            int divisoes = 0;
+//            for (int i = 1; i < numero; i++) {
+//                if (numero % i == 0) {
+//                    divisoes++;
+//                }
+//
+//                if (divisoes > 2) {
+//                    primo = false;
+//                    break;
+//                }
+//            }
+//        }
+//        System.out.println(primo);
+//
+//        // 5 - Inversão de Número
+//        System.out.println("Digite um numero para ser invertido:");
+//        numero = scanner.nextInt();
+//        numeroString = String.valueOf(numero);
+//        String reverso = new StringBuilder(numeroString).reverse().toString();
+//        System.out.println(reverso);
+//
+//        // 6 - Potência
+//        System.out.println("Digite um numero para calcular a potência:");
+//        numero = scanner.nextInt();
+//        int expoente;
+//        System.out.println("Digite um expoente a potência:");
+//        expoente = scanner.nextInt();
+//        resultado = 1;
+//        for (int i = 1; i <= expoente; i++) {
+//            resultado *= numero;
+//        }
+//        System.out.println(resultado);
+//
+//        // 7 - Palíndromo
+//        System.out.println("Digite um numero para verificar se é palíndromo:");
+//        numero = scanner.nextInt();
+//        numeroString = String.valueOf(numero);
+//        reverso = new StringBuilder(numeroString).reverse().toString();
+//        System.out.println(numeroString.equals(reverso));
+//
+//        // 8 - Múltiplos de um Número
+//        System.out.println("Digite um numero para retornar os multiplos:");
+//        numero = scanner.nextInt();
+//        int limite;
+//        System.out.println("Digite um numero para o limite dos multiplos:");
+//        limite = scanner.nextInt();
+//        for (int i = numero; i <= limite; i++) {
+//            if (i % numero == 0) {
+//                System.out.println(i);
+//            }
+//        }
+//
+//        // 9 - Conversão de Binário para Decimal
+//        System.out.println("Digite um numero binario para retornar o valor decimal:");
+//        numeroString = scanner.nextLine();
+//        resultado = 0;
+//        int potencia = 0;
+//        for (int i = numeroString.length() - 1; i >= 0; i--) {
+//            char digito = numeroString.charAt(i);
+//            if (digito == '1') {
+//                resultado += Math.pow(2, potencia);
+//            }
+//            potencia++;
+//        }
+//        System.out.println(resultado);
 
-        // 2 - Fatorial
-        System.out.println("Digite um numero para calcular o fatorial:");
+        // 10 - Série de Fibonacci++
+        System.out.println("Digite um numero para limitar a sequencia de Fibonacci:");
         numero = scanner.nextInt();
-        resultado = 1;
-        do {
-            resultado = resultado * numero;
-            numero--;
-        } while (numero > 1);
-        System.out.println(resultado);
-
-        // 3 - Contagem de Dígitos
-        System.out.println("Digite um numero para receber a quantidade de dígitos:");
-        numero = scanner.nextInt();
-        String numeroString = String.valueOf(Math.abs(numero));
-        System.out.println(numeroString.length());
-
-        // 4 - Números Primos
-        boolean primo = true;
-        System.out.println("Digite um numero para saber se ele é primo:");
-        numero = scanner.nextInt();
-        if (numero < 2) {
-            primo = false;
+        if (numero > 0) {
+            imprimirFibonacciPositivo(numero);
+        } else if (numero < 0) {
+            imprimirFibonacciNegativo(-numero);
         } else {
-            int divisoes = 0;
-            for (int i = 1; i < numero; i++) {
-                if (numero % i == 0) {
-                    divisoes++;
-                }
-
-                if (divisoes > 2) {
-                    primo = false;
-                    break;
-                }
-            }
+            System.out.println(0);
         }
-        System.out.println(primo);
+    }
 
-        // 5 - Inversão de Número
-        System.out.println("Digite um numero para ser invertido:");
-        numero = scanner.nextInt();
-        numeroString = String.valueOf(numero);
-        String reverso = new StringBuilder(numeroString).reverse().toString();
-        System.out.println(reverso);
-
-        // 6 - Potência
-        System.out.println("Digite um numero para calcular a potência:");
-        numero = scanner.nextInt();
-        int expoente;
-        System.out.println("Digite um expoente a potência:");
-        expoente = scanner.nextInt();
-        resultado = 1;
-        for (int i = 1; i <= expoente; i++) {
-            resultado *= numero;
+    public static void imprimirFibonacciPositivo(int numero) {
+        int a = 0;
+        int b = 1;
+        for (int i = 0; i < numero; i++) {
+            System.out.println(a + " ");
+            int proximo = a + b;
+            a = b;
+            b = proximo;
         }
-        System.out.println(resultado);
+    }
 
-        // 7 - Palíndromo
-        System.out.println("Digite um numero para verificar se é palíndromo:");
-        numero = scanner.nextInt();
-        numeroString = String.valueOf(numero);
-        reverso = new StringBuilder(numeroString).reverse().toString();
-        System.out.println(numeroString.equals(reverso));
-
-        // 8 - Múltiplos de um Número
-        System.out.println("Digite um numero para retornar os multiplos:");
-        numero = scanner.nextInt();
-        int limite;
-        System.out.println("Digite um numero para o limite dos multiplos:");
-        limite = scanner.nextInt();
-        for (int i = numero; i <= limite; i++) {
-            if (i % numero == 0) {
-                System.out.println(i);
-            }
+    public static void imprimirFibonacciNegativo(int numero) {
+        int a = 0;
+        int b = 1;
+        for (int i = 0; i < numero; i++) {
+            System.out.println(a + " ");
+            int proximo = b - a;
+            b = a;
+            a = proximo;
         }
-
-        // 9 - Conversão de Binário para Decimal
-        System.out.println("Digite um numero binario para retornar o valor decimal:");
-        numeroString = scanner.nextLine();
-        resultado = 0;
-        int potencia = 0;
-        for (int i = numeroString.length() - 1; i >= 0; i--) {
-            char digito = numeroString.charAt(i);
-            if (digito == '1') {
-                resultado += Math.pow(2, potencia);
-            }
-            potencia++;
-        }
-        System.out.println(resultado);
-
-        // 10 - Série de Fibonacci
     }
 
 }
